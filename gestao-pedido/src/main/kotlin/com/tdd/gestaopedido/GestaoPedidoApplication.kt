7f36ca20ad049e66c93a8637f2ef4a9861b60061
Cesar Okada <cesary@ciandt.com>
2022-05-20 12:12:42 -0300

    Initial commit

---
+++ gestao-pedido/src/main/kotlin/com/tdd/gestaopedido/GestaoPedidoApplication.kt
@@ -0,0 +1,11 @@
+package com.tdd.gestaopedido
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GestaoPedidoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GestaoPedidoApplication>(*args)
+}
---
+++ gestao-pedido/src/test/kotlin/com/tdd/gestaopedido/GestaoPedidoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tdd.gestaopedido
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GestaoPedidoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
